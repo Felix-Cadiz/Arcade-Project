1df0f9abc036b373376c1eb158c1817c518cc596
Felix-Cadiz <FelixRCadiz@gmail.com>
2021-10-07 20:12:50 -0700

    Progress on the arcade project

--- app.js
+++ app.js
@@ -1,48 +1,107 @@
-let initialState = {}
+let state = {}
+const fps = 5
+const boardSpaces = 13
 
-
-// Array of Arrays for Snake body
-// We will UNSHIFT the new direction and POP the tail
-let snake = {
-    body: [ [10, 5], [10, 6], [10, 7], [10, 8] ],
-    nextDirection: [1, 0],
+const resetState = () => {
+    state.board = []
+    buildState ()
+    renderState()
+    food ()
+    state.score = 0
 }
-
-let currentGameState = {
-    apple: [11, 8],
-    snake: snake,
-}
-
-function buildInitialState () {
-    initialState.board = []
-    for (let i = 0; i < 195; i++) {
-        let row = []
-        for (let j = 0; j < 195; j++) {
-            row.push("")
-        }
-        initialState.board.push(row)
-    }
-    console.log(initialState.board)
-}
-
 
 
 let boardElement = document.getElementById("board")
+let scoreElement = document.getElementById("score")
+let startElement = document.createElement("BUTTON")
 
-function renderState () {
-    for (let i = 0; i < initialState.board.length; i++) {
-        let tile = initialState.board[i]
-        let tileElement = document.createElement("div")
-        tileElement.classList.add("tile")
-        tileElement.dataset.index = i
-        boardElement.appendChild(tileElement)
-    }
+
+function score () {
+    scoreElement.innerHTML = `<div>${state.score}</div>`
+}
+
+
+const start = () => {
+    startElement.innerHTML = "Start Game!"
+    document.body.appendChild(startElement)
+    startElement.addEventListener("click", resetState)
 }
 
 
 
-buildInitialState ()
-renderState()
+function setInitialSnake () {
+    let snake = snakeInitial.body
+    // console.log(snake)
+    for (let i = 0; i < snakeInitial.body.length; i++) {
+        let position = snakeInitial.body[i]
+        // console.log(position)
+        // console.log(state.board)
+        state.board[position[0]][position[1]] = "snake"
+    }
+    renderState()
+}
+
+
+// Snake will become a function
+// Array of Arrays for Snake body
+// We will UNSHIFT the new direction and POP the tail
+
+const snakeInitial = {
+    body: [ [0, 6], [1, 6], [2, 6], [3, 6] ],
+}
+
+
+function food () {
+    let appleXAxis = Math.floor(Math.random() * boardSpaces)
+    let appleYAxis = Math.floor(Math.random() * boardSpaces)
+    state.board[appleYAxis][appleXAxis] = "food"
+    console.log (appleXAxis, appleYAxis)
+    renderState()
+}
+
+
+function buildState () {
+    state.board = []
+    for (let i = 0; i < boardSpaces; i++) {
+        let row = []
+        for (let j = 0; j < boardSpaces; j++) {
+            row.push("")
+        }
+        state.board.push(row)
+    }
+    console.log(state.board)
+}
+
+
+function renderState () {
+    boardElement.innerHTML = ""
+    for (let i = 0; i < state.board.length; i++) {
+        for (let j = 0; j < state.board.length; j++) {
+            let tile = state.board[i][j]
+            let tileElement = document.createElement("div")
+            tileElement.innerText = tile
+            tileElement.classList.add("tile")
+            if (tile === "food") {
+                tileElement.classList.add("food")
+                tileElement.classList.remove("tile")
+                tileElement.innerText = ""
+            }
+            if (tile === "snake") {
+                tileElement.classList.add("snake")
+                tileElement.classList.remove("tile")
+                tileElement.innerText = ""
+            }
+            tileElement.dataset.index = [i, j]
+            boardElement.appendChild(tileElement)
+        }
+    }
+}
+
+
+resetState ()
+start ()
+score ()
+setInitialSnake ()
 
 
 
@@ -52,9 +111,6 @@ renderState()
 
 
 
-// function renderState () {
-
-// }
 
 // function onBoardClick () {
 
@@ -68,20 +124,19 @@ renderState()
 
 
 
-
-
-
 // Time Function
 
-// function tick () {
-
-//     renderState ()
-// }
-
-// setInterval (tick, 1000 / 30) 
+function tick () {
+    console.log(tick)
+    renderState ()
+}
 
 
+document.addEventListener("keydown", function (event) {
+    console.log(event.key)
+})
 
+// setInterval (tick, 1000 / fps) 
 
 
 // Action Function

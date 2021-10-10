08330a5b97edbaf434689070152e252adaad9349
Felix-Cadiz <FelixRCadiz@gmail.com>
2021-10-10 14:27:50 -0700

    Play Button Working

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const state = {
     "snakeBody": [ [3, 6], [2, 6], [1, 6], [0, 6] ],
 }
 
-state.snakeHead = state.snakeBody[0]
+state.snakeHead = state.snakeBody[0];
 
 const fps = 3
 const boardSpaces = 13
@@ -19,11 +19,35 @@ const boardSpaces = 13
 
 let boardElement = document.getElementById("board")
 let scoreElement = document.getElementById("score")
-let startElement = document.createElement("BUTTON")
-let playAgainElement = document.createElement("BUTTON")
 let gameOverElement = document.getElementById("gameOver")
+let startElement = document.getElementById("startElement")
+let playAgainElement = document.getElementById("playAgainElement")
 
 // ********** innerHTML Lines **********
+
+function pressButtonStart () {
+    setInterval (tick, 1000 / fps) 
+}
+
+const start = () => {
+    startElement.innerHTML = "Start Game!"
+    document.body.appendChild(startElement)
+    startElement.addEventListener("click", pressButtonStart)
+    startElement.addEventListener("click", increaseSpeed)
+}
+
+// Each click will increase the speed by fps variable by 3.
+const increaseSpeed = () => {
+    if (startElement.innerHTML === "Start Game!") {
+    startElement.innerHTML = "Press to Increase Speed!"
+    }
+}
+
+const playAgain = () => {
+    playAgainElement.innerHTML = "Play Again?"
+    document.body.appendChild(playAgainElement)
+    playAgainElement.addEventListener("click", newGame)
+}
 
 const score = () => {
     if (state.snakeHead === "food") {
@@ -35,19 +59,16 @@ const score = () => {
     }
 }
 
-const start = () => {
-    startElement.innerHTML = "Start Game!"
-    document.body.appendChild(startElement)
-    startElement.addEventListener("click", tick)
+function gameOverParameters() {
+    if (state.board[0] < 0 || state.board[0] > boardSpaces || state.board[1] < 0 || state.board[1] > boardSpaces) {
+        gameOver()
+    }
+    // if (state.snakeBody.includes(state.snakeHead)) {
+    //     gameOver()
+    // }
 }
 
-const playAgain = () => {
-    playAgainElement.innerHTML = "Play Again?"
-    document.body.appendChild(playAgainElement)
-    playAgainElement.addEventListener("click", resetState)
-}
-
-const gameOver = () => {
+function gameOver () {
     gameOverElement.innerHTML = `<div>Game Over. Your final score is ${state.score}. <br> Would you like to play again?</div>`
 }
 
@@ -65,8 +86,7 @@ function buildState () {
     console.log(state.board)
 }
 
-
-function renderState () {
+function renderBoard () {
     boardElement.innerHTML = ""
     for (let i = 0; i < state.board.length; i++) {
         for (let j = 0; j < state.board.length; j++) {
@@ -79,8 +99,9 @@ function renderState () {
                 tileElement.classList.remove("tile")
                 tileElement.innerText = ""
             }
-            const coords = [i,j]
-            if (state.snakeBody.includes(coords) || tile === "snake") {
+            // const coords = [i,j]
+            // state.snakeBody.includes(coords) || 
+            if (tile === "snake") {
                 tileElement.classList.add("snake")
                 tileElement.classList.remove("tile")
                 tileElement.innerText = ""
@@ -88,15 +109,6 @@ function renderState () {
             tileElement.dataset.index = [i, j]
             boardElement.appendChild(tileElement)
         }
-    }
-}
-
-const gameOverParameters = () => {
-    if (state.snakeHead === [-1,true] || [true, -1] || [boardSpaces.length, true] || [true, boardSpaces.length]) {
-        gameOver()
-    }
-    if (snakeBody.includes(state.snakeHead)) {
-        gameOver()
     }
 }
 
@@ -111,9 +123,8 @@ const setInitialSnake = () => {
         // console.log(state.board)
         state.board[position[0]][position[1]] = "snake"
     }
-    renderState()
+    renderBoard()
 }
-
 
 const playerSnake = () => {
     state.snakeBody.pop()
@@ -121,79 +132,88 @@ const playerSnake = () => {
     const newSnakeHeadRow = state.snakeHead[1] + state.direction.row
     state.snakeHead = [newSnakeHeadCol, newSnakeHeadRow]
     state.snakeBody.unshift(state.snakeHead)
+    renderState()
 }
 
 
 // Food is still able to appear on snake
 
-function food () {
+const food = () => {
     let appleColumn = Math.floor(Math.random() * boardSpaces)
     let appleRow = Math.floor(Math.random() * boardSpaces)
-    // console.log (appleRow, appleColumn)
-    state.board[appleRow][appleColumn] = "food"
-    if (state.snakeBody.includes("food")) {
-        food()
-    }
-    renderState()
+    console.log (appleRow, appleColumn)
+    state.board[[appleRow][appleColumn]] = "food"
+    renderBoard()
+    // if (state.snakeBody.includes("food")) {
+    //     food()
+    // }
+    
 }
 
 
-
-
-const resetState = () => {
+const renderState = () => {
     state.board = []
     state.score = 0
     buildState ()
     score ()
-    start ()
-    renderState()
+    renderBoard()
     setInitialSnake ()
-    food ()
-    playAgain ()
+    gameOverParameters()
 }
-
-
-resetState ()
-
-
 
 // ********** Logic ********** 
 
-// Time Function
-
 function tick () {
     console.log(tick)
-    playerSnake()
-    renderState ()
-
+    playerSnake ()
+    renderBoard ()
 }
 
 
-setInterval (tick, 1000 / 3) 
+// ********** Game opening Functions **********
 
+function newGame () {
+    clearInterval(setInterval)
+    const state = {
+        "score": 0,
+        "board": [],
+        "direction": {
+            "col": 1,
+            "row": 0,
+        },
+        "snakeBody": [ [3, 6], [2, 6], [1, 6], [0, 6] ],
+    }
+    buildState ()
+    setInitialSnake()
+    food()
+    start ()
+}
 
+newGame()
+
+// ********** Event Listeners for Movement **********
 
 document.addEventListener("keydown", function (event) {
-    console.log(event.code)
+    // console.log(event.code)
     if (event.key === "ArrowLeft" && !(state.direction.col === 0 && state.direction.row === 1)) {
         // console.log("Left arrow pressed")
         state.direction.col = 0
         state.direction.row = -1
-        console.log(state.direction)
+        // console.log(state.direction)
     } else if (event.key === "ArrowUp" && !(state.direction.col === 1 && state.direction.row === 0)) {
         // console.log("Up arrow pressed")
         state.direction.col = -1
         state.direction.row = 0
-        console.log(state.direction)
+        // console.log(state.direction)
     } else if (event.key === "ArrowRight" && !(state.direction.col === 0 && state.direction.row === -1)) {
         // console.log("Right arrow pressed")
         state.direction.col = 0
         state.direction.row = 1
-        console.log(state.direction)
+        // console.log(state.direction)
     } else if (event.key === "ArrowDown" && !(state.direction.col === -1 && state.direction.row === 0)) {
         // console.log("Down arrow pressed")
         state.direction.col = 1
         state.direction.row = 0
-        console.log(state.direction)
+        // console.log(state.direction)
     }
 })
